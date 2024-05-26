87f8c39262c9c1efeb832a90efa54f44fb60c34f
MUHAMMAD HUSSAIN SHAMIM <142653978+hussainshamim16@users.noreply.github.com>
2024-05-27 03:53:33 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+
+
+
+
+function showMassage(){
+
+        var mass = document.getElementById("massage");
+        mass.style.display="flex";
+        a = false
+   
+    
+
+}
+
+function deletMessege(){
+    var mass = document.getElementById("massage");
+    mass.style.display="none";
+}
